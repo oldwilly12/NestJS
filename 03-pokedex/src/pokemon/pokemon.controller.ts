ce4aae4f670816317d0a74fb67825346bb3757e8
Fracisco Acosta <rwilly203@gmail.com>
2025-01-08 17:58:10 -0600

    pokedex
    pokedex coneccion mongo DB

--- 03-pokedex/src/pokemon/pokemon.controller.ts
+++ 03-pokedex/src/pokemon/pokemon.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -8,6 +8,7 @@ export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Post()
+  // @HttpCode( 200 ) // codigo de respuesta
   create(@Body() createPokemonDto: CreatePokemonDto) {
     return this.pokemonService.create(createPokemonDto);
   }
